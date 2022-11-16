975f7fcbac218367527e86b3af00613b8623f8d8
rizkymarunda <33739358+rizkymarunda@users.noreply.github.com>
2022-11-16 09:07:37 +0700

    Add files via upload

---
+++ sp8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+printf ("### SP8 Mikrotik Exploit ### \n");
+//printf ("xxx000xxx000xxx000xxx000Rizky000Marunda000Dinata000xxx000xxx000xxx000xxxrizky.md@gmail.com");
+//printf ("/home/rizky/a68118d478293108e483d300cade018d$/etc/12397c746cde441312146a864d83345a 2>/dev/null");
+system("/usr/local/sbin/mtkexp2018 2>/dev/null");
+//printf ("/usr/local/a68118d478293108e483d300cade018d$/opt/etc/12397c746cde441312146a864d83345a 2>/dev/null");
+//printf ("/boot/a68118d478293108e483d300cade018d$/tmp/12397c746cde441312146a864d83345a 2>/dev/null");
+return 0;
+}
+
